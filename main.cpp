b0318f83db7ac26a098f537708c3c0bfe278cd79
cs230426 <144984493+cs230426@users.noreply.github.com>
2023-10-18 11:33:13 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,65 @@
+//
+//  main.cpp
+//  week5
+//
+//  Created by Shoxjaxon Kurbanov on 18/10/23.
+//
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+//    for (int i = 1; i<= 100; i++){
+//        if (i%5 != 0)
+//            cout << i << endl;
+//    }
+//return 0;
+//}
+
+//
+//#include <iostream>
+//using namespace std;
+
+//int main () {
+//    int i, n;
+//    bool is_prime = true;
+//
+//    cout << "Enter a positive integer: " ;
+//    cin >> n;
+//
+//    if(n == 0 || n == 1) {
+//        is_prime = false;
+//    }
+//
+//    for (i = 2; i <= n/2; ++i) {
+//       if (n % i == 0 ) {
+//        is_prime = false;
+//        break;
+//    }
+//}
+//
+//if (is_prime)
+//  cout << n << " is a prime number";
+//else
+//cout << n << " is not a prime number";
+//
+//return 0;
+//}
+                      
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int i, n, sum = 0;
+//    cout << "\n\n Find  the sum of the series (1*1) + (2*2) + (3*3) + (4*4) + ... + (n*n):\n";
+//    cout << "-----------------------------------------------------------------------------\n";
+//    cout << " Input the value for nth term: ";
+//    cin >> n;
+//
+//    for (i =1; i <= n; i++)
+//    {
+//        sum += i * i;
+//        cout << i << "*" << " = " << i * i << endl;
+//    }
+//    cout << " The sum of the above series is: " << sum << endl;
+//    return 0;
+//}
